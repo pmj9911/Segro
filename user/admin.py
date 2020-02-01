7df69f7fe4ce6bdcb7392cdd284d2b4659a856ef
shlokashah <shlokashah0603@gmail.com>
2020-02-01 17:19:29 +0530

    simulator created

--- user/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Segro_User
-# Register your models here.
-
-admin.site.register(Segro_User)
--- user/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from user.models import Segro_User
-
-class SegroUserSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Segro_User
-		fields = ['username','user_location']
-
-
